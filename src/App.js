43c5262a6ac0cbd228a829ad2a7ed58ed013028b
Sean Moriarty <spmoriarty1@gmail.com>
2022-06-06 16:04:55 -0700

    figuring out broken code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import './App.css';
 {/* import the zodiac array */}
+import Header from './Header/Header.js';
+import Main from './Main/Main.js';
+import Footer from './Footer/Footer.js';
+import { zodiac } from './data';
+
 
 function App() {
   return (
     <div className="App">
       {/* import and use the Header. Main, and Footer components here */}
-      {/* the Header component needs a string passed in as the `name` prop */}
-      {/* the Footer component needs a year passed in as the `year` prop */}
-      {/* pass the zodiac array imported above to the Main component as a `zodiacs` prop */}
+      <Header />
+      <Main key={zodiac}/>
+      <Footer />
+
     </div>
   );
 }
 
 export default App;
+
+{/* the Header component needs a string passed in as the `name` prop */}
+{/* the Footer component needs a year passed in as the `year` prop */}
+{/* pass the zodiac array imported above to the Main component as a `zodiacs` prop */}
--- src/ZodiacCard/ZodiacCard.js
+++ src/ZodiacCard/ZodiacCard.js
@@ -1,12 +1,13 @@
 import './ZodiacCard.css';
-
 export default function ZodiacCard({ name, dates }) {
   return (
-    <div className='zodiac'>
+    <div className='zodiac-card'>
       <h1>{name}</h1>
       <p>{dates}</p>
+      <img src = {/images/`${name}`.png} />
       {/* <img>{ public/images/`${name}`.png}</img> */}
     </div>
   );
   // public/images/aquarius.png
-  
+}
+
