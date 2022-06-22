daf6f7b66714f69c9521530063a37280ec6490a3
Sean Moriarty <spmoriarty1@gmail.com>
2022-06-22 16:51:41 -0700

    fixing CI errors

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import backgroundImg from '../background.png';
 import './Main.css';
-import ZodiacCard from './ZodiacCard/ZodiacCard';
+import ZodiacCard from '../ZodiacCard/ZodiacCard';
 
 export default function Main({ zodiacs }) {
   return (
